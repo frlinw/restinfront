a61e257f52cbf56a1bf7f5aca6567e4a41149823
frlinw <matthieu.dambrune@gmail.com>
2022-05-20 19:00:49 +0000

    refactor: update import with new filenames

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export { default as Model } from './src/model.js'
-export { default as FieldTypes } from './src/fieldtypes.js'
+export { default as Model } from './src/Model.js'
+export { default as FieldTypes } from './src/Fieldtypes.js'
